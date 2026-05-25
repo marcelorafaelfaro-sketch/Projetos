4d6f4908532e5f534aa46f475950592da07c1df6
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-05-25 15:42:54 -0300

    Programando a calculadora.

---
+++ Calculadora_SENAI.py
@@ -0,0 +1,17 @@
+print("Bem vindo a Calculadora.\n")
+print("[MENU] -> Selecione alguma das opções abaixo.")
+print("{1} Adição.")
+print("{2} Subtração.")
+print("{3}")
+print("{4}")
+print("{5}")
+print("{6}")
+print("{7}")
+print("{8}")
+print("{9}")
+print("{0}")
+while True:
+    escolha = int(input("Informe o número: "))
+    if (escolha == 1):
+        print("ADIÇÃO")
+        

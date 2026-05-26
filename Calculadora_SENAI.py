3ad0d64bf0e0a0b4838d1b6fcb38d00bff1a06a4
marcelorafaelfaro-sketch <marcelorafaelfaro@gmail.com>
2026-05-26 10:52:21 -0300

    Fiz a multiplicaçao e divisao da calculadora, alterei os valores pra aceitarem float.

--- Calculadora_SENAI.py
+++ Calculadora_SENAI.py
@@ -1,15 +1,15 @@
-print("Bem vindo a Calculadora.\n")
+print("Bem vindo a Calculadora 1.0.\n")
 print("[MENU] -> Selecione alguma das opções abaixo.")
 print("{1} Adição.")
 print("{2} Subtração.")
-print("{3}")
-print("{4}")
-print("{5}")
-print("{6}")
-print("{7}")
-print("{8}")
-print("{9}")
-print("{0}")
+print("{3} Multiplicação")
+print("{4} Divisão")
+print("{5} Potência")
+print("{6} Raiz Quadrada")
+print("{7} Porcentagem")
+print("{8} Fatorial")
+print("{9} AJUDA")
+print("{0} SAIR")
 while True:
     escolha = int(input("Informe o número: "))
     if (escolha == 1):
@@ -18,7 +18,31 @@ while True:
         lista_somativa = list(numeros.split(" "))
         soma = 0
         for numero in lista_somativa:
-            soma += int(numero)
+            soma += float(numero)
         print((f"o resultado da soma usando os numeros {numeros} é = {soma}"))
     if (escolha == 2):
-        print("SUBTRAÇÃO ")
+        print("SUBTRAÇÃO")
+        numero_sub = str(input("Informe os numeros que deseja subtrair: "))
+        lista_subtrativa = list(numero_sub.split(" "))
+        subtracao = float(lista_subtrativa[0])
+        for i in range(1, len(lista_subtrativa)):
+            subtracao -= float(lista_subtrativa[i])
+        print(f"O resultado da subtração entre os valores {numero_sub} é igual a {subtracao:.2f}")
+    if (escolha == 3):
+        print("MULTIPLICAÇÃO")
+        numeros_multi = str(input("Informe os números que deseja multiplicar: "))
+        lista_multiplicativa = list(numeros_multi.split(" "))
+        multiplicacao = 1
+        for numeros_multi in lista_multiplicativa:
+            multiplicacao *= float(numeros_multi)
+        print(f"O resultado da multiplicação entre {numeros_multi} é igual a {multiplicacao:.2f}")
+    if (escolha == 4):
+        print("DIVISÃO")
+        numeros_divis = str(input("Informe os numeros que deseja dividir: "))
+        lista_divisiva = list(numeros_divis.split(" "))
+        if lista_divisiva == 0:
+            print("ERRO")
+        else:
+            divisao = float(lista_divisiva[0]) // float(lista_divisiva[1])
+            resto = float(lista_divisiva[0]) % float(lista_divisiva[1])
+        print(f"A divisão entre {numeros_divis} é igual a {divisao:.2f} e o seu resto é {resto:.2f} ")
